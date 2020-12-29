3f2340adbeae9d7c613b76ae0cf99c28016917cc
Hans <hans@pop-os.localdomain>
2020-12-29 22:08:09 +0900

    Made temporary changes

--- src/components/add-todo/add-todo.component.jsx
+++ src/components/add-todo/add-todo.component.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React , { useState } from 'react';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlusSquare } from '@fortawesome/free-solid-svg-icons'
 import './add-todo.styles.css'
 
-const AddTodo = () => (
-    <button className="add-todo">
-        <FontAwesomeIcon icon={faPlusSquare}/> Create a new task
-    </button>
-)
+const AddTodo = () => {
+
+    return(
+       <button className="add-todo">
+            <FontAwesomeIcon icon={faPlusSquare}/> Create a new task
+        </button>
+    )
+}
 
 export default AddTodo;
